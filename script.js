6f83c632f26d9eaa1226403830fc501a4925ea40
raduzhka <ya.raduzhka@gmail.com>
2017-04-19 13:23:52 +0300

    vue

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 $(window).on('load', function(){
+    var app = new Vue({
+        el: '#app',
+        data: {
+            message: 'Hello Vue!'
+        }
+    })
+});
+$(document).ready(function(){
     $.ajax ({
         type: 'GET',
         url: 'http://koskapanbooks.azurewebsites.net/api/Books',
         ContentType: 'application/json',
-        success: function (data){
-            console.log(data);
-            sweetAlert (data[1].id);
-
+        success: function (dataBook){
+            console.log(dataBook);
+            for (var a in dataBook) {
+                console.log(dataBook[a].author);
+                var titleBook = new Vue ({
+                    el: '#titleBook',
+                    data : { titleBook: dataBook[a].title
+                    }
+                })
+            }
         },
         error: function(){
             alert ('fail');
